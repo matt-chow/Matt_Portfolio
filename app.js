64c6975f95c84171794ed010dee20b5f9675032e
Chowtown <blux2703@gmail.com>
2026-02-02 19:32:20 -0500

    nuked

--- app.js
+++
@@ -1,5 +0,0 @@
-// Static site mode: no dynamic parallax, reveal, or project stack behavior.
-(function() {
-	// Intentionally empty: dynamic scrolling and animated effects removed.
-	console.log('Static layout mode: dynamic scrolling disabled');
-})();
